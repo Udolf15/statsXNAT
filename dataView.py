836127d168145421ca56968b155396d3e6bdcf8f
Udolf15 <hashmi.masif@gmail.com>
2020-02-24 20:38:36 +0530

    create the file for viewing the data

---
+++ dataView.py
@@ -0,0 +1,36 @@
+print("\nProcessing............")
+
+import dataFormatter
+
+# View class
+class View:
+
+    formatter = dataFormatter.Formatter()
+    opted = 0
+
+    # Option that are available now
+    def viewOptions(self):
+        
+        print("\n")
+        print("View all the projects visible to you press 1")
+        print("View all the subjects visible to you press 2")
+        print("View the stats of the XNAT visible to you press 3")
+
+        self.opted = int(input("Enter your choice : "))
+        self.viewSelected()
+
+    def viewSelected(self):
+
+        if(self.opted == 1):
+            self.formatter.projectsFormatter()
+        elif(self.opted == 2):
+            self.formatter.subjectsFormatter()
+        elif(self.opted == 3):
+            self.formatter.stats()
+        else:
+            print("Wrong Choice\n")
+            print("Run the program again")
+
+if __name__ == "__main__":
+    view = View()
+    view.viewOptions()
