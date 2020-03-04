220b995843781902bb19fc44fbe33e8cac958283
Udolf15 <hashmi.masif@gmail.com>
2020-03-04 14:04:07 +0530

    unit tests for dataFetcher.py

---
+++ tests/test_dataFetcher.py
@@ -0,0 +1,32 @@
+import pytest
+from src import dataFetcher
+from src import check_network
+
+data_fetcher = dataFetcher.Fetcher()    # Object of the Fetcher class
+
+
+def test_get_projects():
+
+    out = data_fetcher.get_projects()
+    if(check_network.connect()):
+        assert str(type(out)) == "<class 'pyxnat.core.jsonutil.JsonTable'>"
+    else:
+        assert out == None
+
+
+def test_get_subjects():
+
+    out = data_fetcher.get_subjects()
+    if(check_network.connect()):
+        assert str(type(out)) == "<class 'pyxnat.core.jsonutil.JsonTable'>"
+    else:
+        assert out == None
+
+
+def test_get_experiments():
+
+    out = data_fetcher.get_experiments()
+    if(check_network.connect()):
+        assert str(type(out)) == "<class 'pyxnat.core.jsonutil.JsonTable'>"
+    else:
+        assert out == None
