b957e9706547aa5c016f2f17d7f14bd9773bc3be
Udolf15 <hashmi.masif@gmail.com>
2020-03-07 10:16:20 +0530

    import change

--- src/check_network.py
+++ src/check_network.py
@@ -1,11 +1,11 @@
-import urllib.request
+from urllib3 import request
 
 # This file is used to check for network connection during testing
 # If no network connection found the test depending on network will be skipped
 
 def connect():
     try:
-        urllib.request.urlopen('http://google.com') 
+        request.urlopen('http://google.com') 
         return True
     except:
         return False
